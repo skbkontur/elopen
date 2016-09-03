c3b945eb54566162a21b2ebe61e85c798cd498a5
Kirill Melnikov <kir_melnikov@skbkontur.ru>
2016-09-03 17:03:29 +0500

    Now it works

--- public/app.js
+++ public/app.js
@@ -1,31 +1,20 @@
-import moment from 'moment';
-import uiModules from 'ui/modules';
 import uiRoutes from 'ui/routes';
 
 import 'ui/autoload/styles';
 import './less/main.less';
-import overviewTemplate from './templates/index.html';
-import detailTemplate from './templates/detail.html';
+import template from './templates/index.html';
 
 uiRoutes.enable();
+uiRoutes.when('/', {
+    template,
+    resolve: {
+        currentTime($http) {
+            return $http.get('../api/elopen/example').then(function (resp) {
+                return resp.data.time;
+            });
+        }
+    }
+});
 
-uiRoutes
-    .when('/', {
-        template: overviewTemplate,
-        controller: 'elasticsearchStatusController',
-        controllerAs: 'ctrl'
-    })
-    .when('/index/:name', {
-        template: detailTemplate,
-        controller: 'elasticsearchDetailController',
-        controllerAs: 'ctrl'
-    });
-debugger;
 
-uiModules
-    .get('app/elopen', [])
-    .controller('elasticsearchStatusController', function ($http) {
-        $http.get('../api/elasticsearch_status/indices').then((response) => {
-            this.indices = response.data;
-        });
-    });
+require('plugins/elopen/lib/controllers/IndexCtrl.js');
