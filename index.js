99395072e8b1c1871d1196e08b00611fc32310d2
WeslyG <Weslyg22@gmail.com>
2018-05-08 10:41:41 +0500

    Adapted for elk6 backend

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import indexesRoute from './server/routes/indices';
 
 export default function (kibana) {
-    return new kibana.Plugin({
-        require: ['elasticsearch'],
-        name: 'elopen-plugin',
-        uiExports: {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+    name: 'elopen-plugin',
+    uiExports: {
 
-            app: {
-                title: 'Elopen',
-                description: 'An awesome Kibana plugin',
-                main: 'plugins/elopen/app',
-                icon: 'plugins/elopen/icon.svg'
-            },
-        },
+      app: {
+        title: 'Elopen',
+        description: 'An awesome Kibana plugin',
+        main: 'plugins/elopen/app',
+        icon: 'plugins/elopen/icon.svg'
+      },
+    },
 
-        init(server) {
-            indexesRoute(server);
-        }
+    init(server) {
+      indexesRoute(server);
+    }
 
-    });
-};
+  });
+}
--- server/routes/indices.js
+++ server/routes/indices.js
@@ -1,59 +1,51 @@
 const elasticsearch = require('elasticsearch');
 
 export default function (server) {
-    const config = server.config();
-    const client = new elasticsearch.Client({
-        host: config.get('elasticsearch.url'),
-    });
-    server.route({
-        path: '/api/elopen/_stats',
-        method: 'GET',
-        handler(req, reply) {
-            // client.cluster.state({
-            //     metric: 'metadata',
-            //     human: true,
-            // }, function (err, response) {
-            //     reply(
-            //         response
-            //     );
-            // });
-            // TODO: we must change it to client.cluster.state({metric: 'metadata', human: true}, function(err,response){reply(response)}). But it won't work because of mergetIndexName function.
-            client.indices.stats({human: true}, function (err, response) {
-                reply(
-                    response
-                );
-            });
-        }
-    });
-    server.route({
-        path: '/api/elopen/index/{name}',
-        method: 'GET',
-        handler(req, reply) {
-            const name = req.params.name;
-            client.cluster.state({
-                metric: 'metadata',
-                index: name,
-                human: true,
-            }, function (err, response) {
-                reply(
-                    response.metadata.indices[name]
-                );
-            });
-        }
-    });
-    server.route({
-        path: '/api/elopen/index/{name}/_open',
-        method: 'GET',
-        handler(req, reply) {
-            const name = req.params.name;
-            client.indices.open({
-                index: name,
-                human: true,
-            }, function (err, response) {
-                reply(
-                    response.metadata.indices[name]
-                );
-            });
-        }
-    });
+  const config = server.config();
+  const client = new elasticsearch.Client({
+    host: config.get('elasticsearch.url'),
+  });
+  server.route({
+    path: '/api/elopen/_stats',
+    method: 'GET',
+    // переехал на другой метод api
+    handler(req, reply) {
+      client.cluster.state({
+        metric: 'metadata',
+        human: true,
+      }, function (err, response) {
+        reply(response);
+      });
+    }
+  });
+  // не понял зачем нужен маршрут
+  server.route({
+    path: '/api/elopen/index/{name}',
+    method: 'GET',
+    handler(req, reply) {
+      const name = req.params.name;
+      client.cluster.state({
+        metric: 'metadata',
+        index: name,
+        human: true,
+      }, (err, response) => {
+        reply(
+          response.metadata.indices[name]
+        );
+      });
+    }
+  });
+  server.route({
+    path: '/api/elopen/index/{name}/_open',
+    method: 'GET',
+    handler(req, reply) {
+      const name = req.params.name;
+      client.indices.open({
+        index: name,
+        human: true,
+      }, (err, response) => {
+        reply(response.status);
+      });
+    }
+  });
 }
